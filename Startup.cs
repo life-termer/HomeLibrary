133ea9a15dd99c691484d8d9a5f5c47dacab34de
Dmitry Surcin <53041180+life-termer@users.noreply.github.com>
2021-06-02 19:33:26 +0200

    Innitial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeLibrary.Startup))]
+namespace HomeLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
